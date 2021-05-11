26b08c921624b67e98bf9cab3684e1a9cd1f4196
Andrew Mickael <andrew.mickael@gmail.com>
2021-05-10 21:30:46 -0500

    update build & version

--- build.sh
+++ build.sh
@@ -3,4 +3,3 @@
 rm -r build; rm -r *.egg-info; rm -r dist
 pip install --upgrade build twine
 python -m build --sdist --wheel .
-python -m twine upload --non-interactive -u "__token__" -p "$PYPI_TOKEN" --repository testpypi dist/*
